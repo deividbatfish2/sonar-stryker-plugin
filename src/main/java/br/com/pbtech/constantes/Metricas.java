dc086e39d8456d774caf884e735c7cf021f0b867
Deivid Teixeira <deividbatfish2@gmail.com>
2021-01-03 11:28:39 -0300

    Define repository name

--- src/main/java/br/com/pbtech/constantes/Metricas.java
+++ src/main/java/br/com/pbtech/constantes/Metricas.java
@@ -2,4 +2,6 @@ package br.com.pbtech.constantes;
 
 public class Metricas {
     public static final String DOMINIO = "Testes de Mutação";
+    public static final String REPOSITORY_KEY = "stryker";
+    public static final String REPOSITORY_NAME = "Stryker";
 }
