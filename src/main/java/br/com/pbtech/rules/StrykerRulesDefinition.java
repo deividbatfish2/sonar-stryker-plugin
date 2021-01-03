61914d697a0259e5a9b1d27ca2ac19d2ecdf0988
Deivid Teixeira <deividbatfish2@gmail.com>
2021-01-03 11:31:40 -0300

    Adiciona regra para registro de mutantes  vivos

--- src/main/java/br/com/pbtech/rules/StrykerRulesDefinition.java
+++ src/main/java/br/com/pbtech/rules/StrykerRulesDefinition.java
@@ -1,54 +1,48 @@
 package br.com.pbtech.rules;
 
+import org.sonar.api.rule.RuleKey;
 import org.sonar.api.rule.RuleStatus;
 import org.sonar.api.rule.Severity;
 import org.sonar.api.rules.RuleType;
 import org.sonar.api.server.rule.RulesDefinition;
 
+import static br.com.pbtech.constantes.Languages.JAVASCRIPT_KEY;
+import static br.com.pbtech.constantes.Metricas.REPOSITORY_KEY;
+import static br.com.pbtech.constantes.Metricas.REPOSITORY_NAME;
+
 public class StrykerRulesDefinition implements RulesDefinition {
 
-    private final String REPOSITORY_NAME = "Stryker";
-    private final String REPOSITORY_KEY = "stryker";
-    private static final String SURVIVED_MUTANT_RULE_KEY = "stryker.survived.mutant";
-    private static final String INSUFFICIENT_MUTATION_COVERAGE_RULE_KEY = "stryker.insufficient.mutation.coverage";
+    public static final RuleKey INSUFFICIENT_MUTATION_COVERAGE_RULE_KEY = RuleKey.of(REPOSITORY_KEY,"stryker.insufficient.mutation.coverage");
+    public static final RuleKey MUTANTE_VIVO_POS_TESTE = RuleKey.of(REPOSITORY_KEY, "stryker.mutante.vivo.pos.teste");
     private static final String COVERAGE_RATIO_PARAM = "minimumMutationCoverageRatio";
 
     public static final String TAG_TEST_QUALITY = "test-quality";
-    public static final String TAG_TEST_COVERAGE = "test-coverage";
+    public static final String TAG_TEST_COVERAGE = "coverage";
 
     @Override
     public void define(Context context) {
         NewRepository repository = context
-                .createRepository(REPOSITORY_KEY, "javascript")
+                .createRepository(REPOSITORY_KEY, JAVASCRIPT_KEY)
                 .setName(REPOSITORY_NAME);
 
-        /*
-         * Rule: Survived Mutant
-         * Current thinking is that a survived mutant is at least as severe as missing code coverage, probably more severe.
-         * Reason for more severe: a test covers this code, so there may be a false sense of security regarding test coverage
-         */
-        repository.createRule(SURVIVED_MUTANT_RULE_KEY)
-                .setName("Survived mutant")
-                .setHtmlDescription(
-                        "An issue is created when an existing test fails to identify a mutation in the code. For more information, review the <a href=\"http://pitest.org/quickstart/mutators\">PIT documentation</a>")
+        repository.createRule(MUTANTE_VIVO_POS_TESTE.rule())
+                .setName("Stryker - Mtante vivo pos teste")
+                .setHtmlDescription("Um mutante sobrevieu após a execução dos testes.")
                 .setStatus(RuleStatus.READY)
                 .setSeverity(Severity.MAJOR)
-                .setType(RuleType.BUG)
-                .setTags(TAG_TEST_QUALITY)
-                .setActivatedByDefault(false);
+                .setType(RuleType.CODE_SMELL)
+                .setTags(TAG_TEST_QUALITY, TAG_TEST_COVERAGE)
+                .setActivatedByDefault(true);
 
-        /*
-         * Rule: Insufficient Mutation coverage
-         */
-        NewRule insufficientMutationCoverageRule = repository.createRule(INSUFFICIENT_MUTATION_COVERAGE_RULE_KEY)
-                .setName("Insufficient mutation coverage")
+        NewRule insufficientMutationCoverageRule = repository.createRule(INSUFFICIENT_MUTATION_COVERAGE_RULE_KEY.rule())
+                .setName("Stryker - Insufficient mutation coverage")
                 .setHtmlDescription(
                         "An issue is created on a file as soon as the mutation coverage on this file is less than the required threshold. It gives the number of mutations to be covered in order to reach the required threshold.")
-                .setStatus(RuleStatus.READY)
+                .setStatus(RuleStatus.BETA)
                 .setSeverity(Severity.MAJOR)
                 .setType(RuleType.BUG)
                 .setTags(TAG_TEST_QUALITY, TAG_TEST_COVERAGE)
-                .setActivatedByDefault(false);
+                .setActivatedByDefault(true);
 
         insufficientMutationCoverageRule
                 .createParam(COVERAGE_RATIO_PARAM)
